84b7cea507ea2221eeb5904eb5dd654855ca837a
Terence Sun <tsun@lbl.gov>
2013-07-12 16:53:00 -0700

    Made file sizes pretty

--- tough/templatetags/file_filters.py
+++ tough/templatetags/file_filters.py
@@ -3,6 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name="as_mb")
-def as_mb(value):
-    return "%0.3f MB" % (float(value)/1000000)
+@register.filter(name="size_nice")
+def size_nice(value):
+    if int(value) < 1000:
+        return "%0.0f B" % (float(value))
+    if int(value) < 100000:
+        return "%0.2f kB" % (float(value)/1000)
+    else:
+        return "%0.2f MB" % (float(value)/1000000)

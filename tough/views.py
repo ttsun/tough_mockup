3d542a135ccfdb45968281bd162cc8f20072d709
Terence Sun <tsun@lbl.gov>
2013-07-16 14:25:14 -0700

    Fixed bug in view

--- tough/views.py
+++ tough/views.py
@@ -39,7 +39,7 @@ def report_error(request):
 def tail_file(request, job_id, path):
     job = get_object_or_404(Job, pk=job_id)
     file_url = job.jobdir + path
-    return render_to_response("tail.html", {"file_url": file_url} context_instance=RequestContext(request))
+    return render_to_response("tail.html", {"file_url": file_url}, context_instance=RequestContext(request))
 
 
 def submit(request, job_id):

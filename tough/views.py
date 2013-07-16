68ebc7b3681768e4c46f7b508f13caa3a3f1e058
Terence Sun <tsun@lbl.gov>
2013-07-16 15:09:16 -0700

    updated base for tail view

--- tough/views.py
+++ tough/views.py
@@ -36,7 +36,7 @@ def report_error(request):
                               context_instance=RequestContext(request))
 
 
-def tail_file(request, job_id, path):
+def view_file(request, job_id, path):
     job = get_object_or_404(Job, pk=job_id)
     file_url = job.jobdir + path
     return render_to_response("tail.html", {"file_url": file_url}, context_instance=RequestContext(request))
